e00b7972f4d33668ceeca796758443f492f30ef9
Seth Holobaugh <seth.holobaugh@gmail.com>
2024-11-10 20:28:36 -0600

    fix(profile): add under construction ;

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h1>Profile</h1>
+      <h2
+        className='mt-8'
+      >
+        This page is under construction...
+      </h2>
     </div>
   )
 };
