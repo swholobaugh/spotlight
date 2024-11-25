7a89a0c293f8a976d2794ced9c6ab356fffd3a2c
Seth Holobaugh <seth.holobaugh@gmail.com>
2024-11-24 22:29:22 -0600

    fix(hall): update hall of fame

--- src/pages/Hall/Hall.jsx
+++ src/pages/Hall/Hall.jsx
@@ -7,9 +7,13 @@ const Hall = () => {
 
   return (
     <div className='flex flex-col justify-center items-center'>
-      <h1 className='text-3xl text-app-accent font-bold mb-4'>Hall of Fame</h1>
-      <section className='w-2/3 mt-4 p-6'>
-        <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-3xl text-app-accent font-bold mb-2'>Hall of Fame</h1>
+      <section className='w-2/3 p-6'>
+        <div className='flex flex-col justify-center text-center items-center'>
+          <div className='mb-4 text-[#2C2C2C]'>
+            <p className='text-lg font-bold'>Welcome to the Hall of Fame!</p>
+            <p className='text-lg'>Here you can view the previous nominees and winners of the Aggie Spotlight.</p>
+          </div>
           <header className='text-lg font-bold text-[#500000] mb-4'>
             Previous Nominees
           </header>
